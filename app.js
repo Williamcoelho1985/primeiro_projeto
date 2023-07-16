f06c8e546b0c524fe60d355248b7b2b6367ef299
Williamcoelho1985 <coelho2015@hotmail.com>
2023-07-16 14:31:54 -0300

    Aprendendo a retornar ao arquivo passado

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 console.log(" Rodando o sistema de gerenciamenot de jogos"")
 console.log("A aplicação está se conectando ao banco de dados")
-
+console.log ("aprendendo a voltar a aplicação anterior")
